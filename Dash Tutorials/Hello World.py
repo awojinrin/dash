0f71e2afa520a8f121a422908ad70b7ce97a187c
awojinrin <awojinringbengathompson@gmail.com>
2022-09-27 11:53:55 +0100

    Hello Dash

---
+++ Dash Tutorials/Hello World.py
@@ -0,0 +1,48 @@
+import dash
+from dash.dependencies import Input, Output
+from dash import dcc, html
+
+from pandas_datareader import data as web
+from datetime import datetime as dt
+
+external_stylesheets = [
+    {
+        "href": 'https://codepen.io/chriddyp/pen/bWLwgP.css',
+        "rel": "stylesheet",
+    },
+]
+
+app = dash.Dash('Hello World', external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+], style={'width': '500'})
+
+@app.callback(
+    Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value,
+        'yahoo',
+        dt(2017, 1, 1),
+        dt.now()
+    )
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
